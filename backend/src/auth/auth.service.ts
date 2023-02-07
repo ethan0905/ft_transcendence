b7735fd4b388ac98e18e91f4e03774dcfc7b8d5f
Safar Ethan <ethansafar@gmail.com>
2023-02-07 13:20:11 +0100

    worked on signin funcion + implemented a makefile to launch backend properly

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -34,7 +34,25 @@ export class AuthService{
 		}
 	}
 	
-	signin() {
-		return {msg: 'I have signed in!'};
+	async signin(dto: AuthDto) {
+		//1. find the user by email
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email, 
+			},
+		});
+		//if user does not exist, throw exception
+		if (!user)
+			throw new ForbiddenException('Credentials incorrect');
+
+		//2. compare password
+		const pwMatches = await argon.verify(user.hash, dto.password);
+		//if password incorrect, throw exception
+		if (!pwMatches)
+			throw new ForbiddenException('Credentials incorrect');
+
+		//3. send back the user
+		delete user.hash;
+		return user;
 	}
 }
