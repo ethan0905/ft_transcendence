9b588ecc6fda6e29f1e266b3ebe92b91c59f3ea8
Chi-khang DUONG <chduong@e1r8p6.clusters.42paris.fr>
2023-04-13 19:35:15 +0200

    clean css

--- frontend/src/components/Chat/UserList/UserItems.tsx
+++ frontend/src/components/Chat/UserList/UserItems.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef,  Component} from 'react';
 import UserAvatar from "./UserAvatar";
 import "./UserList.css";
 
-
 type DropdownOption = {
   label: string;
   value: string;
@@ -26,7 +25,6 @@ const KickBanMuteButton: React.FC = () => {
     setSelectedOption(option);
     setShowDropdown(false);
     // Call to action (kick, ban or mute)
-
   };
 
   return (
@@ -48,7 +46,6 @@ const KickBanMuteButton: React.FC = () => {
     </div>
   );
 };
-
 
 interface Props {
   animationDelay: number;
@@ -72,20 +69,17 @@ export default class UserItems extends Component<Props> {
   
     render() {
       return (
-        <div
-          style={{ animationDelay: `0.${this.props.animationDelay}s` }}
+        <div style={{ animationDelay: `0.${this.props.animationDelay}s` }}
           onClick={this.selectChat}
           className={`userlist__item ${ this.props.active ? this.props.active : "" } `}
         >
           <div className='id_user'>
-            <UserAvatar
-              image={this.props.image ? this.props.image : "http://placehold.it/80x80"}/>
+            <UserAvatar image={this.props.image ? this.props.image : "http://placehold.it/80x80"}/>
     
             <div className="userMeta">
               <p>{this.props.name}</p>
             </div>
           </div>
-          
 
           <KickBanMuteButton/>
         </div>
