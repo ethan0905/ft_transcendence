33de2dbda98b2a11c848bc7a62b8eb4b6845c5a3
Chi-khang DUONG <chduong@e1r11p6.clusters.42paris.fr>
2023-04-15 00:32:36 +0200

    2faVerif Page

--- frontend/src/pages/2faVerify.tsx
+++ frontend/src/pages/2faVerify.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import AuthCode from 'react-auth-code-input';
+import './2faVerify.css';
 
 function Verify2FA() {
 	const [twoFACode, setTwoFACode] = React.useState('');
-
 	const handleOnChange = (code: string) => {setTwoFACode(code);};
 
 	async function check2FACode(): Promise<any> {
@@ -23,22 +23,13 @@ function Verify2FA() {
 	}
 
 return (
-	<div className='2faVerifyPage' style={{
-		width: '100%',
-		height: '100vh',
-		backgroundSize: 'cover',
-		backgroundImage: `url(https://m.media-amazon.com/images/M/MV5BNTliMGU4ZDAtN2JlYy00ZTBiLWFlMDEtNTFiMDIyYmYyMTA1L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg)`,
-		backgroundRepeat: 'no-repeat',
-		backgroundPosition: 'center',
-		display: 'flex',
-		flexDirection: 'column',
-		// justifyContent: 'center',
-		alignItems: 'center',
-		fontFamily: 'Kocak',
-		color: 'white',
-	}}>
+	<div className='VerifyPage'>
 		<h1>Two-Factor Authentication</h1>
-		<AuthCode allowedCharacters='numeric' onChange={handleOnChange}/>
+		<AuthCode 
+			inputClassName='Authcode' 
+			allowedCharacters='numeric' 
+			onChange={handleOnChange}
+		/>
 		<button onClick={check2FACode}>Submit code</button>
 	</div>
 	);
