91c6f6f4945d20f0b7a6932ec7226ad85b683d2f
Narguss <alexandre.chanelaw@icloud.com>
2023-04-22 14:53:52 +0200

    Cleanup

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,4 +1,3 @@
-import { UseFilters } from '@nestjs/common';
 import {
 	ConnectedSocket,
 	MessageBody,
@@ -6,46 +5,16 @@ import {
 	WebSocketGateway,
 	WebSocketServer,
   OnGatewayConnection,
-  OnGatewayDisconnect
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 import { ChannelCreateDto } from './dto/create-chat.dto';
 import { ChannelMessageSendDto, DmMsgSend  } from './dto/msg.dto';
 import { ValidationPipe, UsePipes } from '@nestjs/common';
-import {
-	updateChat,
-} from './type/chat.type';
 import { UserService } from 'src/user/user.service';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { QuitChanDto, JoinChanDto, EditChannelCreateDto } from "./dto/edit-chat.dto"
 
-// @WebSocketGateway()
-// export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-//   @WebSocketServer() server: Server;
-//   users: number = 0;
-
-//   constructor() {
-//     this.server = new Server();
-  
-
-//    handleConnection() {
-//     // A client has connected
-//     this.users++;
-//     // Notify connected clients of current users
-//     this.server.emit('users', this.users);
-//   }
-//    handleDisconnect() {
-//     // A client has disconnected
-//     this.users--;
-//     // Notify connected clients of current users
-//     this.server.emit('users', this.users);
-//   }
-//   @SubscribeMessage('chat')
-//    onChat(client, message) {
-//     client.broadcast.emit('chat', message);
-//   }
-// }
 
 export interface User {
   id: number;
@@ -98,14 +67,6 @@ export class ChatGateway implements OnGatewayConnection {
     }
   }
 
-  // async handleJoinSocket(id: number, @ConnectedSocket() client: Socket) {
-	// 	// const channels = await this.chatService.get__channelsUserIn(id);
-	// 	// await client.join('default_all');
-	// 	// if (channels)
-	// 	// 	for (const channel of channels) {
-	// 	// 		await client.join(channel);
-	// 	// 	}
-	// }
 
   handleDisconnect(client: Socket) {
     console.log("Disconnect")
@@ -134,21 +95,6 @@ export class ChatGateway implements OnGatewayConnection {
       return "error";
     this.server.to(data.chatId.toString()).emit("NewMessage",chat); // emit to the room
   }
-
-  // @SubscribeMessage('sendMsgtoC')
-  // async MsgtoUser(
-  //   @MessageBody()  data: DmMsgSend ,
-  //   @ConnectedSocket() client : Socket,
-  // ) {
-  //   const user = await this.prisma.user.findUnique({
-  //     where : {
-  //       email : data.mail,
-  //     },
-  //   }
-  //   )
-  //   const chat = await this.chatService.newDM(data, user.id);
-  //   console.log("cMsg added");
-  // }
 
   @SubscribeMessage('join')
   async join_chan(
@@ -270,19 +216,4 @@ export class ChatGateway implements OnGatewayConnection {
       console.log("chan updated");
   }
 
-  // // @SubscribeMessage('updateChat')
-  // // @UsePipes(new ValidationPipe())
-  // // async updateChat(
-  // //   @MessageBody() data: updateChat,
-  // //   @ConnectedSocket() client: Socket,
-  // // ) {
-  // //   try {
-
-    
-  // //     await this.chatService.updateChat(data);
-  // //     client.broadcast.emit('updateChat', data);
-  // //   }
-  // //   catch {
-  // //     client.broadcast.emit('fail to updtae', data)
-  // //   }
 }
